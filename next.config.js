1fa565c9468e5a0af4b69ec88e4a867f0fb91d87
Adri x Fahmi <fahmigutawan@gmail.com>
2023-06-27 23:25:31 +0700

    Add firebase to repository.tsx

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env:{
+        NEXT_FIREBASE_apiKey:process.env.NEXT_FIREBASE_apiKey,
+        NEXT_FIREBASE_authDomain:process.env.NEXT_FIREBASE_authDomain,
+        NEXT_FIREBASE_projectId:process.env.NEXT_FIREBASE_projectId,
+        NEXT_FIREBASE_storageBucket:process.env.NEXT_FIREBASE_storageBucket,
+        NEXT_FIREBASE_messagingSenderId:process.env.NEXT_FIREBASE_messagingSenderId,
+        NEXT_FIREBASE_appId:process.env.NEXT_FIREBASE_appId
+    }
+}
 
 module.exports = nextConfig
---
+++ src/repository/repository.tsx
@@ -0,0 +1,23 @@
+import {FirebaseApp, initializeApp} from "firebase/app";
+import {Auth, getAuth} from "firebase/auth";
+import {Firestore, getFirestore} from "firebase/firestore";
+
+class Repository {
+    fbAuth: Auth
+    fbFirestore: Firestore
+
+    constructor() {
+        const firebaseConfig = {
+            apiKey: process.env.NEXT_FIREBASE_apiKey,
+            authDomain: process.env.NEXT_FIREBASE_authDomain,
+            projectId: process.env.NEXT_FIREBASE_projectId,
+            storageBucket: process.env.NEXT_FIREBASE_storageBucket,
+            messagingSenderId: process.env.NEXT_FIREBASE_messagingSenderId,
+            appId: process.env.NEXT_FIREBASE_appId
+        };
+        const app = initializeApp(firebaseConfig);
+
+        this.fbAuth = getAuth(app)
+        this.fbFirestore = getFirestore(app)
+    }
+}
