5e91255aa79a608592d2ed7831eac75f7785df17
Adri x Fahmi <fahmigutawan@gmail.com>
2023-06-28 00:23:54 +0700

    Change firebaseConfig goes directly inside initializeApp(HERE)

--- src/repository/repository.tsx
+++ src/repository/repository.tsx
@@ -7,15 +7,16 @@ class Repository {
     fbFirestore: Firestore
 
     constructor() {
-        const firebaseConfig = {
-            apiKey: process.env.NEXT_FIREBASE_apiKey,
-            authDomain: process.env.NEXT_FIREBASE_authDomain,
-            projectId: process.env.NEXT_FIREBASE_projectId,
-            storageBucket: process.env.NEXT_FIREBASE_storageBucket,
-            messagingSenderId: process.env.NEXT_FIREBASE_messagingSenderId,
-            appId: process.env.NEXT_FIREBASE_appId
-        };
-        const app = initializeApp(firebaseConfig);
+        const app = initializeApp(
+            {
+                apiKey: process.env.NEXT_FIREBASE_apiKey,
+                authDomain: process.env.NEXT_FIREBASE_authDomain,
+                projectId: process.env.NEXT_FIREBASE_projectId,
+                storageBucket: process.env.NEXT_FIREBASE_storageBucket,
+                messagingSenderId: process.env.NEXT_FIREBASE_messagingSenderId,
+                appId: process.env.NEXT_FIREBASE_appId
+            }
+        )
 
         this.fbAuth = getAuth(app)
         this.fbFirestore = getFirestore(app)
