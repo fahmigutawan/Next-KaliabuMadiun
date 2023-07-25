77638c403588d3eb84a858a97bef1e141381ff25
Nauval Fahreza A <nauvalfahreza03@gmail.com>
2023-07-25 16:06:51 +0700

    add loading

---
+++ src/component/base/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="w-full h-screen flex items-center justify-center text-2xl font-bold text-secondary800">
+            Loading . . .
+        </div>
+    )
+}
+
+export default Loading
