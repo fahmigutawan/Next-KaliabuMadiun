8b01cc9d6e26896daaebd0a169a9eb8f185f6e8a
fahmirumagutawan <fahmigutawan@gmail.com>
2023-07-26 13:15:04 +0700

    Built

--- src/app/u/information/gallery/page.tsx
+++ src/app/u/information/gallery/page.tsx
@@ -107,7 +107,7 @@ export default function GalleryPage() {
             <h2 className="mb-9 text-secondary900 font-bold text-4xl">Galeri</h2>
             <div className="grid grid-cols-4 gap-5">
                 {galleryData?.map((data, index) => (
-                    <div className='h-[200px] flex flex-col'>
+                    <div key={data.id} className='h-[200px] flex flex-col'>
                         <div className='flex-1 bg-secondary300 w-full bg-cover bg-center' style={{backgroundImage: `url(${data.url})`}}>
                         </div>
                         <div className='w-full py-4 bg-secondary800 font-semibold text-white flex items-center justify-center'>{data.description}</div>
