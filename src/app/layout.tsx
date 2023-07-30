7ee5b613591c537c21ee62e728109a8eaf2b4774
fahmirumagutawan <fahmigutawan@gmail.com>
2023-07-30 12:39:54 +0700

    Add head on main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,31 @@ import { Toaster } from 'react-hot-toast';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href="/kab_madiun.png" sizes="any" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="theme-color" content="#ffffff" />
+                <title>Website Resmi Desa Kaliabu, Madiun</title>
+                <meta name="description" content="Website Resmi Desa Kaliabu, Madiun" />
+                <meta name="keywords" content="Website Resmi Desa Kaliabu, Madiun" />
+                <meta name="author" content="Kaliabu Madiun" />
+                <meta name="application-name" content="Website Resmi Desa Kaliabu, Madiun" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta
+                    name="apple-mobile-web-app-title"
+                    content="Website Resmi Desa Kaliabu, Madiun"
+                />
+                <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+                <meta name="format-detection" content="telephone=no" />
+                <meta name="mobile-web-app-capable" content="yes" />
+                <meta name="msapplication-TileColor" content="#ffffff" />
+                <meta name="msapplication-tap-highlight" content="no" />
+                <meta name="robots" content="index, follow" />
+            </head>
             <AppProvider>
                 <body className={`h-[100vh] bg-white`}>
                     {children}
-                    <Toaster/>
+                    <Toaster />
                 </body>
             </AppProvider>
         </html>
