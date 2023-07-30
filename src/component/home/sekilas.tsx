f0500abc8f9010ce23840787704f1d8536d533a0
fahmirumagutawan <fahmigutawan@gmail.com>
2023-07-31 00:32:26 +0700

    Added text to sekilas

--- src/component/home/sekilas.tsx
+++ src/component/home/sekilas.tsx
@@ -10,7 +10,7 @@ export const HomeSekilas = () => {
                 <div className='w-1/2 h-full flex flex-col justify-center'>
                     <p className='text-[20px] px-[8px] text-black font-semibold'>Sekilas</p>
                     <p className='text-[24px] px-[8px] text-black font-bold bg-slate-300'>Desa Kaliabu</p>
-                    <p className='text-black'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu velit lacus. Mauris tincidunt aliquet velit, ac mattis leo auctor vel. Mauris vitae vehicula justo, in tempus lectus. Curabitur euismod neque at libero efficitur hendrerit. Fusce accumsan sollicitudin arcu ut sodales. Curabitur sit amet urna rutrum, fermentum metus ac, venenatis dolor. Praesent et tellus elit. Vestibulum viverra malesuada mi sit amet lacinia.</p>
+                    <p className='text-black'>Secara administratif, Desa Kaliabu terletak di wilayah Kecamatan Mejayan Kabupaten Madiun dengan posisi dibatasi oleh wilayah desa-desa tetangga. Di sebelah utara berbatasan dengan Desa Bancong Kecamatan Wonoasri, di sisi selatan berbatasan dengan hutan, sedangkan di sisi timur berbatasan dengan Desa Klecorejo.</p>
                 </div>
             </div>
             <div className="md:hidden flex flex-col gap-4">
@@ -24,7 +24,7 @@ export const HomeSekilas = () => {
                 <div className="w-full">
                     <div className="w-full aspect-video bg-slate-500" />
                 </div>
-                <p className='text-black text-xs text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu velit lacus. Mauris tincidunt aliquet velit, ac mattis leo auctor vel. Mauris vitae vehicula justo, in tempus lectus. Curabitur euismod neque at libero efficitur hendrerit. Fusce accumsan sollicitudin arcu ut sodales. Curabitur sit amet urna rutrum, fermentum metus ac, venenatis dolor. Praesent et tellus elit. Vestibulum viverra malesuada mi sit amet lacinia.</p>
+                <p className='text-black text-xs text-justify'>Secara administratif, Desa Kaliabu terletak di wilayah Kecamatan Mejayan Kabupaten Madiun dengan posisi dibatasi oleh wilayah desa-desa tetangga. Di sebelah utara berbatasan dengan Desa Bancong Kecamatan Wonoasri, di sisi selatan berbatasan dengan hutan, sedangkan di sisi timur berbatasan dengan Desa Klecorejo.</p>
             </div>
         </>
     )
